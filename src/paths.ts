db76d0ec1654571b30a591776d0abf85796bb81b
xeroxis-xs <txsheng0609@hotmail.com>
2024-06-04 02:20:39 +0800

    fix linting errors

--- src/paths.ts
+++ src/paths.ts
@@ -1,6 +1,10 @@
 export const paths = {
   home: '/',
-  auth: { signIn: '/auth/sign-in', signUp: '/auth/sign-up', resetPassword: '/auth/reset-password' },
+  auth: {
+    signIn: '/auth/sign-in', 
+    // signUp: '/auth/sign-up',
+    // resetPassword: '/auth/reset-password'
+  },
   dashboard: {
     overview: '/dashboard',
     account: '/dashboard/account',
