6632a03b4aeadb45e0e59185de293188be3eebf9
xeroxis-xs <txsheng0609@hotmail.com>
2024-08-06 23:06:56 +0800

    fix env var

--- src/api/micro-service.ts
+++ src/api/micro-service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_MICROSERVICE_URL,
+  baseURL: 'https://eduquest-microservice.azurewebsites.net',
 })
 
 export default api;
