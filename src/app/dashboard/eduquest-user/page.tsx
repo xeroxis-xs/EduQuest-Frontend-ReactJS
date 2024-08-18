aca9cb0b6a1c4f04c68c111300072d9137b026db
Teoh Xi Sheng <txsheng0609@hotmail.com>
2024-08-18 21:26:18 +0800

    remove create user form

--- src/app/dashboard/eduquest-user/page.tsx
+++ src/app/dashboard/eduquest-user/page.tsx
@@ -3,11 +3,8 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
-import { Download as DownloadIcon } from '@phosphor-icons/react/dist/ssr/Download';
 import { Plus as PlusIcon } from '@phosphor-icons/react/dist/ssr/Plus';
-import { Upload as UploadIcon } from '@phosphor-icons/react/dist/ssr/Upload';
 import { XCircle as XCircleIcon } from '@phosphor-icons/react/dist/ssr/XCircle';
-// import { EduquestUserFilters } from '@/components/dashboard/eduquest-user/eduquest-user-filters';
 import { EduquestUserTable } from '@/components/dashboard/eduquest-user/eduquest-user-table';
 import type { EduquestUser } from '@/types/eduquest-user';
 import apiService from "@/api/api-service";
@@ -15,15 +12,14 @@ import {AxiosError, type AxiosResponse} from "axios";
 import { logger } from '@/lib/default-logger'
 import {authClient} from "@/lib/auth/client";
 import { EduquestUserForm } from "@/components/dashboard/eduquest-user/eduquest-user-form";
-import { Box } from '@mui/material';
 
 export default function Page(): React.JSX.Element {
   const [eduquestUsers, setEduquestUsers] = React.useState<EduquestUser[]>([]);
-  const [showForm, setShowForm] = React.useState(false);
-
-  const toggleForm = (): void => {
-    setShowForm(!showForm);
-  };
+  // const [showForm, setShowForm] = React.useState(false);
+  //
+  // const toggleForm = (): void => {
+  //   setShowForm(!showForm);
+  // };
 
   const getEduquestUser = async (): Promise<void> => {
     try {
@@ -55,14 +51,14 @@ export default function Page(): React.JSX.Element {
       <Stack direction="row" spacing={1} justifyContent="space-between">
           <Typography variant="h4">Eduquest Users</Typography>
 
-          <Button startIcon={showForm ? <XCircleIcon fontSize="var(--icon-fontSize-md)"/> :
-            <PlusIcon fontSize="var(--icon-fontSize-md)"/>} variant="contained" onClick={toggleForm}>
-            {showForm ? 'Close' : 'Add'}
-          </Button>
+          {/*<Button startIcon={showForm ? <XCircleIcon fontSize="var(--icon-fontSize-md)"/> :*/}
+          {/*  <PlusIcon fontSize="var(--icon-fontSize-md)"/>} variant="contained" onClick={toggleForm}>*/}
+          {/*  {showForm ? 'Close' : 'Add'}*/}
+          {/*</Button>*/}
 
 
       </Stack>
-      {showForm ? <EduquestUserForm onCreateSuccess={getEduquestUser} /> : null}
+      {/*{showForm ? <EduquestUserForm onCreateSuccess={getEduquestUser} /> : null}*/}
       {/*<EduquestUserFilters/>*/}
       <EduquestUserTable rows={eduquestUsers}/>
     </Stack>
