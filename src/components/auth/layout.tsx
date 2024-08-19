4cfdb061c99f4972ee90a4146d2860dde4fd33ba
xeroxis-xs <txsheng0609@hotmail.com>
2024-08-20 03:58:32 +0800

    update login page

--- src/components/auth/layout.tsx
+++ src/components/auth/layout.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
 import RouterLink from 'next/link';
-import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
-import { ListChecks, Crown, Trophy } from "@phosphor-icons/react/dist/ssr";
+import Box from '@mui/material/Box'
 
 import { paths } from '@/paths';
 import { DynamicLogo } from '@/components/core/logo';
@@ -20,6 +17,10 @@ export function Layout({ children }: LayoutProps): React.JSX.Element {
         flexDirection: 'column',
         gridTemplateColumns: '1fr 1fr',
         minHeight: '100%',
+        backgroundImage: 'url(/assets/login_background.svg)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
       }}
     >
       <Box sx={{ display: 'flex', flex: '1 1 auto', flexDirection: 'column' }}>
@@ -29,60 +30,53 @@ export function Layout({ children }: LayoutProps): React.JSX.Element {
           </Box>
         </Box>
         <Box sx={{ alignItems: 'center', display: 'flex', flex: '1 1 auto', justifyContent: 'center', p: 3 }}>
-          <Box sx={{ maxWidth: '450px', width: '100%' }}>{children}</Box>
+          <Box sx={{ maxWidth: '450px', width: '100%' }}>
+            {children}
+          </Box>
         </Box>
       </Box>
-      <Box
-        sx={{
-          alignItems: 'center',
-          background: 'radial-gradient(50% 50% at 50% 50%, #122647 0%, #090E23 100%)',
-          color: 'var(--mui-palette-common-white)',
-          display: { xs: 'none', lg: 'flex' },
-          justifyContent: 'center',
-          p: 3,
-        }}
-      >
-        <Stack spacing={3}>
-          <Stack spacing={1}>
-            <Typography color="inherit" sx={{ fontSize: '24px', lineHeight: '32px', textAlign: 'center' }} variant="h1">
-              Welcome to{' '}
-              <Box component="span" sx={{ color: '#15b79e' }}>
-                EduQuest
-              </Box>
-            </Typography>
-            <Typography align="center" variant="subtitle1">
-              A platform to for students to learn and earn!
-            </Typography>
-          </Stack>
-          <Stack spacing={2}>
-            <Box display="flex" justifyContent="center" alignItems="center">
-              <ListChecks size={28}  />
-              <Typography align="center" variant="subtitle1" sx={{ marginLeft: '8px' }}>
-                Complete
-              </Typography>
-            </Box>
-            <Box display="flex" justifyContent="center" alignItems="center">
-              <Crown size={28} />
-            <Typography align="center" variant="subtitle1" sx={{ marginLeft: '8px' }}>
-               Conquer
-            </Typography>
-            </Box>
-            <Box display="flex" justifyContent="center" alignItems="center">
-              <Trophy size={28} />
-            <Typography align="center" variant="subtitle1" sx={{ marginLeft: '8px' }}>
-               Collect
-            </Typography>
-            </Box>
-          </Stack>
-          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-            <Box
-              component="img"
-              alt="Widgets"
-              src="/assets/auth-widgets.png"
-              sx={{ height: 'auto', width: '100%', maxWidth: '600px' }}
-            />
-          </Box>
-        </Stack>
+      <Box>
+        {/*<Stack spacing={3}>*/}
+        {/*  <Stack spacing={1}>*/}
+        {/*    <Typography color="inherit" sx={{ fontSize: '24px', lineHeight: '32px', textAlign: 'center' }} variant="h1">*/}
+        {/*      Welcome to{' '}*/}
+        {/*      <Box component="span" sx={{ color: '#15b79e' }}>*/}
+        {/*        EduQuest*/}
+        {/*      </Box>*/}
+        {/*    </Typography>*/}
+        {/*    <Typography align="center" variant="subtitle1">*/}
+        {/*      A platform to for students to learn and earn!*/}
+        {/*    </Typography>*/}
+        {/*  </Stack>*/}
+        {/*  <Stack spacing={2}>*/}
+        {/*    <Box display="flex" justifyContent="center" alignItems="center">*/}
+        {/*      <ListChecks size={28}  />*/}
+        {/*      <Typography align="center" variant="subtitle1" sx={{ marginLeft: '8px' }}>*/}
+        {/*        Complete*/}
+        {/*      </Typography>*/}
+        {/*    </Box>*/}
+        {/*    <Box display="flex" justifyContent="center" alignItems="center">*/}
+        {/*      <Crown size={28} />*/}
+        {/*    <Typography align="center" variant="subtitle1" sx={{ marginLeft: '8px' }}>*/}
+        {/*       Conquer*/}
+        {/*    </Typography>*/}
+        {/*    </Box>*/}
+        {/*    <Box display="flex" justifyContent="center" alignItems="center">*/}
+        {/*      <Trophy size={28} />*/}
+        {/*    <Typography align="center" variant="subtitle1" sx={{ marginLeft: '8px' }}>*/}
+        {/*       Collect*/}
+        {/*    </Typography>*/}
+        {/*    </Box>*/}
+        {/*  </Stack>*/}
+        {/*  <Box sx={{ display: 'flex', justifyContent: 'center' }}>*/}
+        {/*    <Box*/}
+        {/*      component="img"*/}
+        {/*      alt="Widgets"*/}
+        {/*      src="/assets/auth-widgets.png"*/}
+        {/*      sx={{ height: 'auto', width: '100%', maxWidth: '600px' }}*/}
+        {/*    />*/}
+        {/*  </Box>*/}
+        {/*</Stack>*/}
       </Box>
     </Box>
   );
--- src/components/auth/sign-in-form.tsx
+++ src/components/auth/sign-in-form.tsx
@@ -1,26 +1,54 @@
+'use client';
 import * as React from 'react';
 import Alert from '@mui/material/Alert';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import { SignInButton } from '@/components/auth/sign-in-button';
+import { useTheme } from '@mui/material/styles';
+import { CheckSquareOffset as CheckSquareOffsetIcon } from "@phosphor-icons/react/dist/ssr/CheckSquareOffset";
+import { Ranking as RankingIcon } from "@phosphor-icons/react/dist/ssr/Ranking";
+import { Medal as MedalIcon } from "@phosphor-icons/react/dist/ssr/Medal";
+
+import Box from "@mui/material/Box";
 
 export function SignInForm(): React.JSX.Element {
+  const theme = useTheme();
 
   return (
-    <Stack spacing={4}>
-      <Stack spacing={2}>
-        <SignInButton />
-      </Stack>
-      <Stack spacing={2}>
-      <Alert color="warning">
+    <Stack spacing={4} >
+      <Typography align="center" variant="h6">
+        Welcome to EduQuest!
+      </Typography>
+      <Typography align="center" variant="body1">
+        A platform for you to learn and earn.
+      </Typography>
+      <SignInButton />
+      <Alert severity="info">
         Only users with {' '}
         <Typography component="span" sx={{ fontWeight: 700 }} variant="inherit">
           NTU 365 Account
         </Typography>{' '}
         have access to this site.
-
       </Alert>
-      </Stack>
+
+      <Box sx={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+        <Stack component="ul" direction="row" spacing={2} style={{ listStyleType: 'none', paddingLeft: 0, marginTop: 0, justifyContent: 'space-evenly', width: '100%' }}>
+          <li style={{ display: 'flex', alignItems: 'center' }}>
+            <CheckSquareOffsetIcon size={20} style={{ flexShrink: 0, marginRight: '8px' }} color={theme.palette.primary.main} />
+            <Typography variant="body1">Complete</Typography>
+          </li>
+          <li style={{ display: 'flex', alignItems: 'center' }}>
+            <RankingIcon size={20} style={{ flexShrink: 0, marginRight: '8px' }} color={theme.palette.primary.main} />
+            <Typography variant="body1">Compete</Typography>
+          </li>
+          <li style={{ display: 'flex', alignItems: 'center' }}>
+            <MedalIcon size={20} style={{ flexShrink: 0, marginRight: '8px' }} color={theme.palette.primary.main} />
+            <Typography variant="body1">Collect</Typography>
+          </li>
+        </Stack>
+      </Box>
+
+
     </Stack>
   );
 }
